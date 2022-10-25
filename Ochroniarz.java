acb11252f28d416dabbd5dd3840aeb946e5c7367
Tomasz <tomaszmikulka@gmail.com>
2022-10-25 17:03:35 +0200

    Zadanie pierwsze

---
+++ Ochroniarz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ochroniarz {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String imie=sc.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String nazwisko=sc.nextLine();
+        System.out.println("Gdzie mieszkasz?");
+        String miasto=sc.nextLine();
+    }
+}

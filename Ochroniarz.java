2409a267146a19e988a77c18f484fce0cd1bd2d2
Tomasz <tomaszmikulka@gmail.com>
2022-10-25 17:20:18 +0200

    Zadanie pierwsze

--- Ochroniarz.java
+++ Ochroniarz.java
@@ -7,7 +7,13 @@ public class Ochroniarz {
         String imie=sc.nextLine();
         System.out.println("Jak masz na nazwisko?");
         String nazwisko=sc.nextLine();
-        System.out.println("Gdzie mieszkasz?");
+        System.out.println("Miasto zamieszkania?");
         String miasto=sc.nextLine();
+        System.out.println("Ile masz lat?");
+        int wiek=sc.nextInt();
+        System.out.println("Podaj stan cywilny?");
+        String stan_cywilny=sc.nextLine();
+
+        System.out.println(imie + "\n" + nazwisko + "\n" + miasto + "\n" + wiek + "\n" + stan_cywilny);
     }
 }
